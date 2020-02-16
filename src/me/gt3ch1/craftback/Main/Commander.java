466820adbd5611a981abcaec1f2253a27c138e55
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-15 19:31:39 -0700

    Added log for command sent.

--- src/me/gt3ch1/craftback/Main/Commander.java
+++ src/me/gt3ch1/craftback/Main/Commander.java
@@ -4,6 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Commander extends BukkitRunnable {
 	private final JavaPlugin plugin;
 	public Commander(JavaPlugin plugin) {
@@ -14,6 +16,7 @@ public class Commander extends BukkitRunnable {
 		Bukkit.getScheduler().runTask(this.plugin, () -> {
 			if(Main.ss != null)
 				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), Main.ss);
+			Bukkit.getLogger().info(ChatColor.BLUE + "[[CraftBack]] Command recieved: " + Main.ss);
 			Main.ss = null;
         });
 	}
