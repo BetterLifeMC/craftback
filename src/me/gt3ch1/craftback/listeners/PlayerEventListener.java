432551993a78f9713fe9c52c8f33a4f15ad39267
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-16 08:43:10 -0700

    What does this look like?

--- src/me/gt3ch1/craftback/listeners/PlayerEventListener.java
+++ src/me/gt3ch1/craftback/listeners/PlayerEventListener.java
@@ -19,7 +19,7 @@ public class PlayerEventListener implements Listener{
 		Player p = event.getPlayer();
 //		p.sendMessage("I am testing this.");
 		craftback.addPlayerToArrayLists(p);
-		System.out.println(craftback.playerArrayList.toString());
+		System.out.println(craftback.playerUUIDAndNameList.toString());
 
 	}
 	@EventHandler
